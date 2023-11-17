5dc67c4ba6efd74aefa33f066bcac52b46348547
WeszeRita <weszelovszkirita@gmail.com>
2023-11-17 16:44:33 +0100

    all review comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { ITodo } from '../models/todo.model';
 import { TodoFacadeService } from '../service/todo-facade.service';
 import { Observable } from 'rxjs';
-
+// ezen a branchen van az összes reviews komment
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
